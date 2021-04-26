80a5d281ba001d259688a9692c12aa2429929a0f
Ennvaweb <35902574+ennva@users.noreply.github.com>
2021-04-26 20:52:57 +0200

    Scafolding

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+ <header class="text-center bg-blue-800 text-white p-4 mb-10">
+   Covid-19 Tracker
+ </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
